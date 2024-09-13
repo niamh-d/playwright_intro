de7f4fadeb5ed1fcebbe30ebabffd1f2abb7cf41
Niamh <niamh.linguist@gmail.com>
2024-09-13 15:07:47 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# playwright_intro
+
+App built with Angular and TypeScript in order to practice tetsing with Playwright from course [*Playwright: Web Automation Testing From Zero to Hero*](https://www.udemy.com/course/playwright-from-zero-to-hero).
+
+Original repo [**here**](https://github.com/bondar-artem/pw-practice-app).
---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,14 @@
+import { test } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("http://localhost:4200/");
+  await page.getByText("Forms").click();
+});
+
+test("the first test", async ({ page }) => {
+  await page.getByText("Form Layouts").click();
+});
+
+test("navigate to datepicker page", async ({ page }) => {
+  await page.getByText("Datepicker").click();
+});
